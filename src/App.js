67dd0a3b7634929c177b85502a7e1d2b68bbaf26
Your Name <you@example.com>
2018-08-22 11:17:16 +0200

    zadanie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles/main.scss';
 import Checkbox from './modules/checkbox';
 
+
 class App extends Component {
   render() {
     return (
--- src/modules/checkbox/index.js
+++ src/modules/checkbox/index.js
@@ -1,18 +1,74 @@
 /* eslint-disable */
 import React from 'react';
 
+
+// class Hidden extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//         }
+//     }
+//     render(){
+//         return <div className="hidden-content">
+//             hidden content
+//         </div>
+//     }
+// }
+//
+// class Checkbox extends React.Component {
+//     render() {
+//         return(
+//             <div className="checkbox">
+//                 <div className="title">
+//                     Checkbox
+//                 </div>
+//                 <div className="content">
+//                     <label className="switch">
+//                     <input type="checkbox" />
+//                     <span className="slider round"> </span>
+//                     </label>
+//                 </div>
+//                 <Hidden/>
+//             </div>
+//         );
+//     }
+// }
+
 class Checkbox extends React.Component {
+    constructor() {
+        super();
+
+        this.state = { checked: false };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        this.setState({
+            checked: !this.state.checked
+        })
+    }
+
     render() {
-        return(
-            <div className="checkbox">
-                <div className="title">
-                    Checkbox
-                </div>
+        const content = this.state.checked
+            ? <div className="hidden-content"> hidden content </div>
+            : null;
+
+        return <div className="checkbox">
+            Checkbox
+                    <div className="center">
+                        <label className="switch">
+                            <input
+                                type="checkbox"
+                                checked={ this.state.checked }
+                                onChange={ this.handleChange } />
+                            <span className="slider round"> </span>
+                        </label>
                 <div className="content">
-                    <input type="checkbox" />
-                </div>
+           </div>
             </div>
-        );
+
+            { content }
+        </div>;
     }
 }
 
